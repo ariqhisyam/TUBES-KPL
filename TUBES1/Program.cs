826c89ce8fbd33e3370661914da3322d9d096fad
Ariq Hisyam Nabil <ariqhisyam62@gmail.com>
2025-04-05 13:41:05 +0700

    menambahkan class DataMahasiswa_103022300034

--- TUBES1/Program.cs
+++ TUBES1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Threading.Tasks;
+using TUBES1;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var dataMahasiswa = new DataMahasiswa103022300034();
+        var filePath = "D:/Figma Hamda/Campursari/tp7_1_103022300034.json";
+        await dataMahasiswa.ReadJsonAsync(filePath);
+    }
+}
