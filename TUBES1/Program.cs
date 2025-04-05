965f86f75fa0b4b2976cb21d576bcc0c5cc0c60b
Ariq Hisyam Nabil <ariqhisyam62@gmail.com>
2025-04-05 14:10:29 +0700

    menambahkan class KuliahMahasiswa103022300034

--- TUBES1/Program.cs
+++ TUBES1/Program.cs
@@ -7,7 +7,11 @@ class Program
     static async Task Main(string[] args)
     {
         var dataMahasiswa = new DataMahasiswa103022300034();
-        var filePath = "D:/Figma Hamda/Campursari/tp7_1_103022300034.json";
-        await dataMahasiswa.ReadJsonAsync(filePath);
+        var mahasiswaFilePath = "D:/Figma Hamda/Campursari/tp7_1_103022300034.json";
+        await dataMahasiswa.ReadJsonAsync(mahasiswaFilePath);
+
+        var kuliahMahasiswa = new KuliahMahasiswa103022300034();
+        var kuliahFilePath = "D:/Figma Hamda/Campursari/tp7_2_103022300034.json";
+        await kuliahMahasiswa.ReadJSON(kuliahFilePath);
     }
 }
