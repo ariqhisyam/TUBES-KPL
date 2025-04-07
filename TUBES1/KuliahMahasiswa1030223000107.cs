93ae48049c1105087ece8ed943265f8c8fe69b7f
relinggaa laptop <linggayorfel@gmail.com>
2025-04-07 14:25:55 +0700

    menambahkan class KuliahMahasiswa103022300034

---
+++ TUBES1/KuliahMahasiswa1030223000107.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TUBES1
+{
+    internal class KuliahMahasiswa1030223000107
+    {
+        public class Course
+        {
+            public string code { get; set; }
+            public string name { get; set; }
+        }
+
+        public class CourseList
+        {
+            public List<Course> courses { get; set; }
+        }
+
+        public async Task ReadJSON(string filePath)
+        {
+            try
+            {
+                using FileStream openStream = File.OpenRead(filePath);
+                var courseList = await JsonSerializer.DeserializeAsync<CourseList>(openStream);
+
+                if (courseList != null && courseList.courses != null)
+                {
+                    Console.WriteLine("Daftar mata kuliah yang diambil:");
+                    for (int i = 0; i < courseList.courses.Count; i++)
+                    {
+                        var course = courseList.courses[i];
+                        Console.WriteLine($"MK {i + 1} {course.code} - {course.name}");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while reading the JSON file: {ex.Message}");
+            }
+        }
+    }
+}
